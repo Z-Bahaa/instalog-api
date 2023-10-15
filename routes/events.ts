84f9e1bbb65db32e8c058fe5c8aba71cf626d5d4
Z-Bahaa <zizobahaapersonal@gmail.com>
2023-10-15 11:15:21 +0200

    adding simple sse event

--- routes/events.ts
+++ routes/events.ts
@@ -35,6 +35,47 @@ EventsRouter.get('/', async (req: Request & {query: any}, res: any) => {
 
 })
 
+EventsRouter.get('/lookups/', async (req: Request & {query: any}, res: any) => { 
+
+  const data = {
+    users: [
+      {
+        "actor_id": "user_3VG742j9PUA2",
+        "actor_name": "zeyad bahaa",
+        "group": "facebook.com",
+      },
+      {
+        "actor_id": "user_3VG742j9PUA2",
+        "actor_name": "ali salah",
+        "group": "instatus.com",
+      },
+      {
+        "actor_id": "user_3VG742j9PUA2",
+        "actor_name": "sami omar",
+        "group": "google.com",
+      },
+    ],
+    actions: [
+      {
+        "name": "user.searched_activity_log_events"
+      },
+      {
+        "name": "user.login_success"
+      },
+      {
+        "name": "user.logout_success"
+      },
+      {
+        "name": "user.login_failure"
+      },
+      {
+        "name": "user.create_post_success"
+      }
+    ]
+  }
+  res.json(data)
+})
+
 EventsRouter.get('/export', async (req: Request & {query: any}, res: any) => { 
 
   try {
@@ -90,49 +131,88 @@ EventsRouter.get('/live', async (req: Request, res: any) => {
   res.setHeader('Content-Type', 'text/event-stream; charset=utf-8')
   // res.setHeader('Access-Control-Allow-Origin', '*')
 
-  const data = {
-    "event": {  
-      "actor_id": "user_3VG742j9PUA2",
-      "actor_name": "zeyad bahaa",
-      "group": "instatus.com",
-      "action": {
-        "name": "user.searched_activity_log_events"
-      },
-      "target_id": "user_DOKVD1U3L031",
-      "target_name": "zeyad@instatus.com",
-      "location": "105.40.62.95",
-      "occurred_at": new Date().toISOString(),
-      "metadata": {
-        "redirect": "/setup",
-        "description": "User Searched Activity Log Events.",
-        "x_request_id": "req_W4Y47lljg85H"
-      }
-   }
-  }
-  const prismaEvent = await instalog.createEvent(data.event)
-  res.write(`data: ${prismaEvent}\n\n`)
-
   const intervalId = setInterval(async () => {
     const data = {
-      "event": {  
-        "actor_id": "user_3VG742j9PUA2",
-        "actor_name": "zeyad bahaa",
-        "group": "instatus.com",
-        "action": {
-          "name": "user.searched_activity_log_events"
+      users: [
+        {
+          "actor_id": "user_3VG742k9PUA2",
+          "actor_name": "zeyad bahaa",
+          "group": "facebook.com",
+          "target_name": "sami@google.com",
+          "target_id": "user_DOKwD1U3L031", 
+          "location": "105.40.62.95",
         },
-        "target_id": "user_DOKVD1U3L031",
-        "target_name": "zeyad@instatus.com",
-        "location": "105.40.62.95",
-        "occurred_at": new Date().toISOString(),
-        "metadata": {
-          "redirect": "/setup",
+        {
+          "actor_id": "user_3VG7p2j9PUA2",
+          "actor_name": "ali salah",
+          "group": "instatus.com",
+          "target_name": "zeyad@facebook.com",
+           "target_id": "user_DOKVD1UpL031", 
+          "location": "189.90.26.15",
+        },
+        {
+          "actor_id": "user_3VG740j9PUA2",
+          "actor_name": "sami omar",
+          "group": "google.com",
+          "target_name": "ali@instatus.com",
+          "target_id": "user_DOKVD1U3j031", 
+          "location": "151.42.72.12",
+        },
+      ],
+      actions: [
+        {
+          "name": "user.searched_activity_log_events",
+          "redirect": "/event_log",
           "description": "User Searched Activity Log Events.",
           "x_request_id": "req_W4Y47lljg85H"
+        },
+        {
+          "name": "user.login_success",
+          "redirect": "/profile",
+          "description": "User Logged In Successfully",
+          "x_request_id": "req_C4Y47lljj85H"
+        },
+        {
+          "name": "user.logout_success",
+          "redirect": "/landing",
+          "description": "User Logged Out Successfully",
+          "x_request_id": "req_H4Y47lljg85w"
+        },
+        {
+          "name": "user.login_failure",
+          "redirect": "/setup",
+          "description": "User Login Failure",
+          "x_request_id": "req_K4Y47lljg85H"
+        },
+        {
+          "name": "user.create_post_success",
+          "redirect": "/post",
+          "description": "User Created New Post Successfully.",
+          "x_request_id": "req_O4Y47lljg85H"
         }
-     }
+      ]
     }
-    const prismaEvent = await instalog.createEvent(data.event)
+    
+    const randomUser = data.users[Math.floor(Math.random() * data.users.length)];
+    const randomAction = data.actions[Math.floor(Math.random() * data.actions.length)];
+
+    const prismaEvent = await instalog.createEvent({  
+      "actor_id": randomUser.actor_id,
+      "actor_name": randomUser.actor_name,
+      "group": randomUser.group,
+      "action": {
+        "name": randomAction.name,
+      },
+      "target_id": randomUser.target_id,
+      "target_name": randomUser.target_name,
+      "location": randomUser.location,
+      "occurred_at": new Date().toISOString(),
+      "metadata": {
+        "redirect": randomAction.redirect,
+        "description": randomAction.description,
+        "x_request_id": randomAction.x_request_id
+      }
+   })
     res.write(`data: ${prismaEvent}\n\n`)
   }, 30000)
 
